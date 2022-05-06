38fca9861da34f1f69eff7a124cf312981b61d5c
Jamie Gardner <jamie@thatgardnerone.com>
2022-05-06 13:30:24 +0100

    Add project files.

---
+++ StoreDataLocally/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace StoreDataLocally;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ StoreDataLocally/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace StoreDataLocally;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
