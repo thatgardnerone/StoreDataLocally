7a912fe5e4e22e2aa53e529e4608f2eb16780667
Jamie Gardner <jamie@thatgardnerone.com>
2022-05-06 13:36:41 +0100

    Add a file access helper

--- StoreDataLocally/App.xaml.cs
+++ StoreDataLocally/App.xaml.cs
@@ -1,7 +1,14 @@
-﻿namespace StoreDataLocally;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StoreDataLocally;
 
 public partial class App : Application
 {
+	string dbPath => FileAccessHelper.GetLocalFilePath("people.db3");
+
 	public App()
 	{
 		InitializeComponent();
